7e32a9629ea1223c77ebbf6c0a552cd64e2729ce
namhaidorjj <mglcoc102@gmail.com>
2024-04-25 13:10:02 +0800

    status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>hello</div>
+      <div>hey whats happening</div>
+      <Link href="/about">
+        <a className="text-blue-500 underline">About</a>
+      </Link>
     </main>
   );
 }
